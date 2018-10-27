4b573b3eb3d1ba3a542166f4f81a5a0ee0b0ee14
joneseyit <joneseyit@protonmail.com>
2018-10-27 17:18:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 require "pry"
-hash = { 
+hash = {
   "season 30": [
     {
       "name":      "Beth Smalls",
@@ -46,5 +46,5 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season_number, contestant_array|
     binding.pry
   end
-  
+
 end
